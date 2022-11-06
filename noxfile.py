daaa8069d832cd56121fc409e910f24958f5b2d8
Martin Jespersen <martincjespersen@gmail.com>
2022-11-06 21:23:57 +0100

    minor

--- noxfile.py
+++ noxfile.py
@@ -172,7 +172,7 @@ def tests(session: Session) -> None:
 @session(python=python_versions[0])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
-    args = session.posargs or ["report"]
+    args = session.posargs or ["report", "-i"]
 
     session.install("coverage[toml]")
 
